99a626bea0eb786dfef77181173255ea0ce8d04b
juan10-7 <juansantana1@colegioguanenta.edu.co>
2025-02-13 16:00:41 -0500

    se agregaron nuevos archivos

--- quiz.py
+++ quiz.py
@@ -6,7 +6,21 @@ print("----- VERIFICAR TRIANGULO ----")
 print("------------------------------")
 
 # input
+a=int(input("agrege el valor a"))
+b=int(input("agrege el valor b"))
+c=int(input("agrege el valor c"))
+
 
 # processing
 
+if a+b>=c:
+    r = " se puede formar el triangulo"
+elif b+c>=a:
+    r = " se puede formar un triangulo"
+elif a+c>=b:
+    r = " se puede formar un triangulo"
+else:
+    r = "el triangulo no se puede hacer"
+
 # output
+print(r)
